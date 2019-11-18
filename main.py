7e3267164615424fe60b3cdab25fde2f0496d06c
MaxenceGiraud <maxence9giraud@gmail.com>
2019-11-18 22:04:43 +0100

    can compute nb of neighbors of all cells

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compute_cell(support,x,y):
-    return support[x,y] == True
+def next_step(support):
+    '''
+    Any live cell with two or three neighbors survives.
+    Any dead cell with three live neighbors becomes a live cell.
+    All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+    '''
 
+    ## Compute the number of neighbour for each cell
+    neighbors = np.zeros(support.shape)
+    neighbors[1:-1,1:-1] = (support[:-2,:-2] + support[:-2,1:-1] + support[:-2,2:] + 
+                        support[1:-1,:-2] +                support[1:-1,2:]  + 
+                        support[2:,:-2]   + support[2:,1:-1]  + support[2:,2:]) 
+
+    return neighbors
 
 def main():
-    support = np.ones((2, 2), dtype=bool)
-    print(compute_cell(support,1,1))
+    support = np.zeros((4, 4), dtype=bool)
+    support[1,1] = "True"
+    support[1,2] = "True"
+    support[2,1] = "True"
+
+    print(support,"\n",next_step(support))
 
 if __name__ == "__main__":
     main()
