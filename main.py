7e7508e817f5491d05f7acd9cb061f83b82c9994
MaxenceGiraud <maxence9giraud@gmail.com>
2019-11-18 22:32:10 +0100

    Can now compute next step of game

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ def next_step(support):
     '''
 
     ## Compute the number of neighbour for each cell
-    neighbors = np.zeros(support.shape)
-    neighbors[1:-1,1:-1] = (support[:-2,:-2] + support[:-2,1:-1] + support[:-2,2:] + 
-                        support[1:-1,:-2] +                support[1:-1,2:]  + 
-                        support[2:,:-2]   + support[2:,1:-1]  + support[2:,2:]) 
 
-    return neighbors
+    neighbors = np.zeros(support.shape)
+    support_int = support.astype(int)
+    neighbors[1:-1,1:-1] = (support_int[:-2,:-2] + support_int[:-2,1:-1] + support_int[:-2,2:] + support_int[1:-1,:-2] + support_int[1:-1,2:]  + support_int[2:,:-2]   + support_int[2:,1:-1]  + support_int[2:,2:]) 
+    print (neighbors,"\n\n")
+
+    ## Go to next step using the number of neighbors and the value of each cell
+    next = np.logical_or(neighbors==3,np.logical_and(support,neighbors==2))
+
+    return next
 
 def main():
     support = np.zeros((4, 4), dtype=bool)
     support[1,1] = "True"
     support[1,2] = "True"
     support[2,1] = "True"
+    support[2,2] = "True"
+    support[0,0] = "True"
+    support[0,1] = "True"
 
-    print(support,"\n",next_step(support))
-
+    print(support,"\n\n",next_step(support))
+    plt.imshow(support)
+    plt.show()
+    
 if __name__ == "__main__":
     main()
