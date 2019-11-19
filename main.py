1e9c3df7722ddfd16436ba577c5f4da8b8757d97
MaxenceGiraud <maxence9giraud@gmail.com>
2019-11-19 19:28:12 +0100

    Can load a grid from a file with coordonates of alive cells

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 def next_step(support):
     '''
@@ -7,36 +8,55 @@ def next_step(support):
     Any dead cell with three live neighbors becomes a live cell.
     All other live cells die in the next generation. Similarly, all other dead cells stay dead.
     '''
-
     ## Compute the number of neighbour for each cell
 
     neighbors = np.zeros(support.shape)
     support_int = support.astype(int)
     neighbors[1:-1,1:-1] = (support_int[:-2,:-2] + support_int[:-2,1:-1] + support_int[:-2,2:] + support_int[1:-1,:-2] + support_int[1:-1,2:]  + support_int[2:,:-2]   + support_int[2:,1:-1]  + support_int[2:,2:]) 
-    print (neighbors,"\n\n")
 
     ## Go to next step using the number of neighbors and the value of each cell
     next = np.logical_or(neighbors==3,np.logical_and(support,neighbors==2))
 
     return next
 
+def load_grid(file):
+    '''
+    Load a grid from a file
+    '''
+    coordonates =[]
+    with open(file, 'r') as f:
+        content = f.readlines()
+        for lign in content[1:] :
+            coordonates.append([int(lign[0]),int(lign[2])])
+
+    grid = np.zeros((max(np.array(coordonates)[:,0])*10, max(np.array(coordonates)[:,0])*10),dtype=bool)
+    for [x,y] in coordonates :
+        grid[x][y] = True 
+
+    return grid
+
+def game_movie():
+    '''
+    create a movie from a certain grid 
+    '''
+    return
+
 def step_plot(support):
-
+    '''
+    plot one step of the game
+    '''
     plt.imshow(support,cmap="gray")
-    plt.show()
-
+    plt.show()  
 
 def main():
-    support = np.zeros((20, 20), dtype=bool)
-    support[1,1] = "True"
-    support[1,2] = "True"
-    support[2,1] = "True"
-    support[2,2] = "True"
-    support[0,0] = "True"
-    support[0,1] = "True"
 
-    step_plot(support)
-    step_plot(next_step(support))
+    #support2 = np.ones((20, 20),dtype=bool)
+    #print(support2,"\n\n\n")
+    #print(next_step(support2))
+    #step_plot(next_step(support2))
 
+    print(load_grid("./grid1.txt")) 
+    #grid = np.loadtxt("./grid1.txt",dtype=bool)
+    #print(grid)
 if __name__ == "__main__":
     main()
